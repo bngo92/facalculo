c4d5a2cdf4f1ce75f6bffe63825f112f8d92a2d2
Bryan Ngo <bngo92@gmail.com>
2025-05-12 19:12:03 -0700

    Add remaining examples for automation-science-pack

--- src/bin/generate-examples.rs
+++ src/bin/generate-examples.rs
@@ -16,5 +16,60 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             &recipe_rates,
         )?)?,
     )?;
+    fs::write(
+        "examples/copper-plate.json",
+        serde_json::to_string_pretty(&facalculo::generate(
+            "copper-plate",
+            true,
+            &["copper-ore"]
+                .into_iter()
+                .map(ToOwned::to_owned)
+                .collect::<Vec<_>>(),
+            &["copper-plate"]
+                .into_iter()
+                .map(ToOwned::to_owned)
+                .collect(),
+            &recipe_rates,
+        )?)?,
+    )?;
+    fs::write(
+        "examples/iron-ore.json",
+        serde_json::to_string_pretty(&facalculo::generate(
+            "iron-ore",
+            true,
+            &[],
+            &["iron-ore"].into_iter().map(ToOwned::to_owned).collect(),
+            &recipe_rates,
+        )?)?,
+    )?;
+    fs::write(
+        "examples/iron-plate.json",
+        serde_json::to_string_pretty(&facalculo::generate(
+            "iron-plate",
+            true,
+            &["iron-ore"]
+                .into_iter()
+                .map(ToOwned::to_owned)
+                .collect::<Vec<_>>(),
+            &["iron-plate"].into_iter().map(ToOwned::to_owned).collect(),
+            &recipe_rates,
+        )?)?,
+    )?;
+    fs::write(
+        "examples/automation-science-pack.json",
+        serde_json::to_string_pretty(&facalculo::generate(
+            "automation-science-pack",
+            true,
+            &["copper-plate", "iron-plate"]
+                .into_iter()
+                .map(ToOwned::to_owned)
+                .collect::<Vec<_>>(),
+            &["iron-gear-wheel"]
+                .into_iter()
+                .map(ToOwned::to_owned)
+                .collect(),
+            &recipe_rates,
+        )?)?,
+    )?;
     Ok(())
 }
